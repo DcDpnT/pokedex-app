0c29fbd22bd62d94846789b37ff26ca50a10377a
Damien <damien@Air-de-Damien.lan>
2023-05-31 10:43:38 +0200

    pokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+const PokemonCard = () => {
+  return (
+    <div>
+      <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+        <figcaption>bulbasaur</figcaption>
+      </figure>
+    </div>
+  );
+};
+
+export default PokemonCard;
