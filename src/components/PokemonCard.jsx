e0834ba4c12bbd40a498ea3477af5611b164f603
Damien <damien@Air-de-Damien.lan>
2023-06-01 14:54:07 +0200

    props.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,21 @@
-const PokemonCard = ({pokemon}) => {
-    
-    console.log(pokemon);
+import PropTypes from "prop-types";
 
+const PokemonCard = ({pokemon}) => {
+    console.log(pokemon);
     return (
-        <figure>
+      <figure>
         {pokemon.imgSrc != null ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : 
         (<p>???</p>)};
         <figcaption>{pokemon.name}</figcaption>
       </figure>
     );
+};
+
+PokemonCard.propTypes = {
+   pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+   }).isRequired, 
 };
 
 export default PokemonCard;
